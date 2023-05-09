3a28d15fc7ced727659fd27f38e28c5c0c6132df
Matheus Felipe <matheusfelipetp@outlook.com>
2023-05-08 23:34:08 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import Router from './routes';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
+export default function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
